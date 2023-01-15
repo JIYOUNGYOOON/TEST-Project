eb9f0aa28f11a9ee344db3b8c1be811de0ad4047
JIYOUNGYOOON <yooon0417@naver.com>
2023-01-15 21:13:27 +0900

    second commit

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ class Person:
     addr = ""
     SSN = ""
 
+
 John = Person()
 John.age = int(input("Age Input: "))
 John.addr = input("Addr Input: ")
@@ -19,5 +20,8 @@ James.SSN = input("SSN Input: ")
 print("James Age:", James.age)
 print("James Addr:", James.addr)
 
-#수정
-#print("James Name:", James.SSN)
+# 수정
+# print("James Name:", James.SSN)
+
+
+print("수정중")
