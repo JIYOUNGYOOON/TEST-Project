8fa2d2a1842ad4516725d139647534c5ac53243e
JIYOUNGYOOON <yooon0417@naver.com>
2023-01-15 20:20:57 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+class Person:
+    age = 0
+    addr = ""
+    SSN = ""
+
+John = Person()
+John.age = int(input("Age Input: "))
+John.addr = input("Addr Input: ")
+John.SSN = input("SSN Input: ")
+print("John Age:", John.age)
+print("John Addr:", John.addr)
+print("John Name:", John.SSN)
+print()
+
+James = Person()
+James.age = int(input("Age Input: "))
+James.addr = input("Addr Input: ")
+James.SSN = input("SSN Input: ")
+print("James Age:", James.age)
+print("James Addr:", James.addr)
+print("James Name:", James.SSN)
